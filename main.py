5dd5559b22752278db04f701a2ea50641ed2bd39
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-07-10 20:53:14 -0700

    Change main data structure to a set

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import keyboard
 from os import walk
 
 current_folder = "sounds\\nkcreams_glarses"
-
 (dirpath, dirnames, filenames) = next(walk(current_folder))
 filenames = [name.split(".")[0] for name in filenames]
-pressed_keys = []
+pressed_keys = set()
+isLogging = True
 
 
 def hook_on_event(event):
+    key = event.name.lower()
+    if (isLogging):
+        print(event.event_type, key)
     if event.event_type == keyboard.KEY_DOWN:
-        if event.name not in pressed_keys:
-            print(event.event_type, event.name)
+        if key not in pressed_keys:
             playsound(
-                f"{current_folder}\\{event.name if event.name in filenames else 'backspace'}.mp3",
+                f"{current_folder}\\{key if key in filenames else 'a'}.mp3",
                 block=False
             )
-            pressed_keys.append(event.name)
+            pressed_keys.add(key)
     elif event.event_type == keyboard.KEY_UP:
-        print(event.event_type, event.name)
-        if event.name in pressed_keys:
-            pressed_keys.remove(event.name)
+        if key in pressed_keys:
+            pressed_keys.remove(key)
 
 keyboard.hook(hook_on_event)
 keyboard.wait()
