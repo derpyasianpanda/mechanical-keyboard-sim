4b5c28c54b7e7e25fa9ec0e58e5e995bb6b0fcef
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-07-11 01:30:22 -0700

    Update README and Add requirements file

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # Mechanical Keyboard Simulator
 
-It just plays cool keyboard sounds lol
+TL:DR It just plays cool keyboard sounds lol
 
-This program is made using the Python keyboard library. The canonical key names can be found [here](https://github.com/boppreh/keyboard/blob/master/keyboard/_canonical_names.py)
+This is a program allows a user to play specific sounds when typing on a
+keyboard. Specifically, it allows a user to test out a certain mechanical
+keyboard's sounds so that a user can "test" out what a certain switch can sound
+like
+
+Confirmed to work only on Windows.
+If you know of a good Python library that can play sounds cross-platform,
+please tell me (or pull request :D)
+
+## Running the program
+
+Have Python 3.6+ installed and all the dependencies from the requirements.txt
+with `pip install -r requirements.txt`
+
+You can now open the main.py with the Python interpreter (or after opening a
+terminal in the directory you can run the command `python main.py`)
+
+You can now just follow the instructions on the screen
+
+## How to add your own custom sounds
+
+All you need to do is go to the sounds directory and put a folder that contains
+all the sounds. The naming scheme is all lowercased and is often just the
+character or key name. For more specifics, see the passage below
+
+This program is made using the Python keyboard library. The canonical key names
+can be found [here](https://github.com/boppreh/keyboard/blob/master/keyboard/_canonical_names.py)
+
+If anyone is willing to give me a set of keyboard sounds, please contact me on
+Discord at Vel0ciTy#3728. I can then upload them to this repository (If I get
+enough, I might make a separate repository to house them)
--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from playsound import playsound
-import keyboard as kb
 from os import walk
-from random import choice
+import keyboard as kb
 from time import sleep
+from random import choice
+from playsound import playsound
 
 print("Welcome to the Mechanical Keyboard Simulator!\n")
 
-isChoosing = True;
+isChoosing = True
 while isChoosing:
-    print("Please choose a directory (Type the number or Directory Name)")
+    print("Please choose a directory with all the sounds (Number or Name)")
     (_, dirnames, _) = next(walk("sounds"))
     for index, name in enumerate(dirnames):
         print(str(index + 1) + ": ", name)
 
     user_input = input("> ")
-    desired_sounds = "sounds/";
+    sound_folder = "sounds/"
 
     try:
         user_input = int(user_input) - 1
         if user_input < len(dirnames) and user_input > -1:
-            desired_sounds += dirnames[user_input]
+            sound_folder += dirnames[user_input]
     except ValueError:
         if user_input in dirnames:
-            desired_sounds += user_input
+            sound_folder += user_input
 
-    (_, _, filenames) = next(walk(desired_sounds))
+    (_, _, filenames) = next(walk(sound_folder))
     filenames = [name.split(".")[0] for name in filenames if "_" not in name]
     if not filenames:
         print("There are no sounds in this folder! :(\n")
@@ -35,16 +35,20 @@ while isChoosing:
 print("\nWould you like to log your keys (It might lag)? (y/N)")
 isLogging = input("> ").lower() == "y"
 
+# This prevents sound spamming by holding down a key
 pressed_keys = set()
 
 
 def on_event(event):
+    # Key name is lowercased b/c Shift+'letter' results in a capital name
     key = event.name.lower()
     if event.event_type == kb.KEY_DOWN:
         if key not in pressed_keys:
+            # When a key's sound isn't present, a random key noise is played
             playsound(
-                f"{desired_sounds}/{key if key in filenames else choice(filenames)}.mp3",
-                block=False
+                sound_folder +
+                f"/{key if key in filenames else choice(filenames)}.mp3",
+                block=False  # Allows for asynchronous sound playback
             )
             pressed_keys.add(key)
     elif event.event_type == kb.KEY_UP:
@@ -56,6 +60,6 @@ if isLogging:
     kb.hook(lambda e: print(e.event_type, e.name))
 kb.hook(on_event)
 
-print("Enjoy your Keyboard sounds! (Press the escape key to quit)")
+print("\nEnjoy your Keyboard sounds! (Press the escape key to quit)")
 kb.wait("esc")
 print("Application Ended")
